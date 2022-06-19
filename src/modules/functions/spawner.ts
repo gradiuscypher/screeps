0f6593387e44d80fd215741757a9af43b2af2f85
gradiuscypher <gradiuscypher@gmail.com>
2022-06-18 23:10:45 -0700

    modifying spawns

--- src/modules/functions/spawner.ts
+++ src/modules/functions/spawner.ts
@@ -5,7 +5,7 @@ export class Spawner {
         const MAX_HARVESTERS = 2;
         const MAX_UPGRADERS = 4;
         const MAX_MINERS = 2;
-        const MAX_TRANSPORT = 4;
+        const MAX_TRANSPORT = 3;
         const REQ_ENERGY = 200;
 
         // role blueprints
